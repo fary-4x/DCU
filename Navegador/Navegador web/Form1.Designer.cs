f260f34efc84301859f9bcbdf70298893240e9b8
fary-4x <108238700+fary-4x@users.noreply.github.com>
2022-07-08 20:27:35 -0400

    Add files via upload

---
+++ Navegador/Navegador web/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Navegador_web
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAtras = new System.Windows.Forms.ToolStripButton();
+            this.btnDelante = new System.Windows.Forms.ToolStripButton();
+            this.btnRefrescar = new System.Windows.Forms.ToolStripButton();
+            this.btnStop = new System.Windows.Forms.ToolStripButton();
+            this.btnHome = new System.Windows.Forms.ToolStripButton();
+            this.URL = new System.Windows.Forms.ToolStripTextBox();
+            this.btnGo = new System.Windows.Forms.ToolStripButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.cmb = new System.Windows.Forms.ToolStripComboBox();
+            this.btnFavoritos = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAtras,
+            this.btnDelante,
+            this.btnRefrescar,
+            this.btnStop,
+            this.btnHome,
+            this.URL,
+            this.btnGo,
+            this.cmb,
+            this.btnFavoritos});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(952, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(23, 22);
+            this.btnAtras.Text = "toolStripButton1";
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnDelante
+            // 
+            this.btnDelante.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnDelante.Image = ((System.Drawing.Image)(resources.GetObject("btnDelante.Image")));
+            this.btnDelante.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDelante.Name = "btnDelante";
+            this.btnDelante.Size = new System.Drawing.Size(23, 22);
+            this.btnDelante.Text = "toolStripButton1";
+            this.btnDelante.Click += new System.EventHandler(this.btnDelante_Click);
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnRefrescar.Image = ((System.Drawing.Image)(resources.GetObject("btnRefrescar.Image")));
+            this.btnRefrescar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(23, 22);
+            this.btnRefrescar.Text = "toolStripButton1";
+            this.btnRefrescar.Click += new System.EventHandler(this.btnRefrescar_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(23, 22);
+            this.btnStop.Text = "toolStripButton1";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(23, 22);
+            this.btnHome.Text = "toolStripButton1";
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // URL
+            // 
+            this.URL.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.URL.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(600, 25);
+            // 
+            // btnGo
+            // 
+            this.btnGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnGo.Image = ((System.Drawing.Image)(resources.GetObject("btnGo.Image")));
+            this.btnGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(23, 22);
+            this.btnGo.Text = "toolStripButton1";
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 25);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(952, 431);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // cmb
+            // 
+            this.cmb.Name = "cmb";
+            this.cmb.Size = new System.Drawing.Size(121, 25);
+            this.cmb.SelectedIndexChanged += new System.EventHandler(this.cmb_SelectedIndexChanged);
+            this.cmb.Click += new System.EventHandler(this.cmb_Click);
+            // 
+            // btnFavoritos
+            // 
+            this.btnFavoritos.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnFavoritos.Image = ((System.Drawing.Image)(resources.GetObject("btnFavoritos.Image")));
+            this.btnFavoritos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFavoritos.Name = "btnFavoritos";
+            this.btnFavoritos.Size = new System.Drawing.Size(23, 22);
+            this.btnFavoritos.Text = "toolStripButton1";
+            this.btnFavoritos.Click += new System.EventHandler(this.btnFavoritos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 456);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Navegador Web";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAtras;
+        private System.Windows.Forms.ToolStripButton btnDelante;
+        private System.Windows.Forms.ToolStripButton btnRefrescar;
+        private System.Windows.Forms.ToolStripButton btnStop;
+        private System.Windows.Forms.ToolStripButton btnHome;
+        private System.Windows.Forms.ToolStripTextBox URL;
+        private System.Windows.Forms.ToolStripButton btnGo;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripComboBox cmb;
+        private System.Windows.Forms.ToolStripButton btnFavoritos;
+    }
+}
+
---
+++ Navegador/Navegador web/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador_web
+{
+    public partial class Form1 : Form
+    {
+        List<string> Favoritos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(URL.Text);
+        }
+
+        private void btnDelante_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btnRefrescar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void btnFavoritos_Click(object sender, EventArgs e)
+        {
+            Favoritos.Add(webBrowser1.Url.ToString());
+            actualizarcmb();
+        }
+
+        private void actualizarcmb()
+        {
+            foreach(string direccion in Favoritos)
+            {
+                cmb.Items.Add(direccion);
+            }
+        }
+
+        private void cmb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(cmb.SelectedItem.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.bing.com/?setlang=es");
+        }
+    }
+}
